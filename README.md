2d50967699d2adc1d9f2f361a3a0ba64ca31444a
Andida Syahendar <omicron.b3ta@gmail.com>
2014-07-10 15:48:44 +0700

    add bandwidth mode

--- README.md
+++ README.md
@@ -4,19 +4,29 @@
 
 Options are : 
 
-url : your latency probing url
+url : your latency probing url (target to path containing bandwidth.dat file if you use bandwidth mode)
 
-low : your low latency threshold in ms
+lowLat : your low latency threshold in ms
 
-medium : your medium latency threshold in ms
+mediumLat : your medium latency threshold in ms
 
-high : your high latency threshold in ms
+highLat : your high latency threshold in ms
+
+lowBw : your low bandwidth threshold in kB/s
+
+mediumBw : your medium bandwidth threshold in kB/s
+
+highBw : your high bandwidth threshold in kB/s
 
 targetElement : where to append the results (append all of arrOfSrc to this element)
+
+type : `"bandwidth" | "latency" | "bandwidth latency" | "latency bandwidth"`
 
 arrOfSrc is an array containing objects with :
 
 latency : the proper latency for this source >> `"low" | "medium" | "high" | "all"`
+
+bandwidth : the proper bandwidth for this source >> `"low" | "medium" | "high" | "all"`
 
 src : the source of this element
 
--- smoothoperator.js
+++ smoothoperator.js
@@ -1,14 +1,21 @@
 /* Usage */
 /*
+USE NAIVE BANDWIDTH DETERMINATION FOR NOW (fileSize / (latency/2))
+
 options are : 
-url : your latency probing url
-low : your low latency threshold in ms
-medium : your medium latency threshold in ms
-high : your high latency threshold in ms
+url : your latency probing url or where file bandwidth.dat results
+lowLat : your low latency in ms
+mediumLat : your medium latency in ms
+highLat : your high latency in ms
+lowBw : your low bandwidth in kB/s
+mediumBw : your medium bandwidth in kB/s
+highBw : your high bandwidth in kB/s
 targetElemet : where to append the results
+type : type of method
 
 arrOfSrc is an array containing objects with :
 latency : the proper latency for this source
+bandwidth : the proper bandwidth for this source
 src : the source of this element
 tag : the element type
 
@@ -23,6 +30,7 @@ arrOfSrc = [
 ]
 options = {
 	url : '/ping',
+	type : 'latency'
 	low : 150,
 	medium : 300,
 	high : 600,
@@ -37,17 +45,58 @@ options = {
 		$.get(options.url).done(function (data) {
 			// check the threshold
 			var delta = Date.now() - now;
-			var latency;
-			if (delta <= options.low) { // low latency
-				latency = 'low';
-			} else if (delta <= options.medium) { // medium latency
-				latency = 'medium';
-			} else if (delta <= options.high || delta > options.high) { // high latency
-				latency = 'high';
+			var latency, bw;
+			if (options.type === 'latency') {
+				if (delta <= options.lowLat) { // low latency
+					latency = 'low';
+				} else if (delta <= options.mediumLat) { // medium latency
+					latency = 'medium';
+				} else if (delta <= options.highLat || delta > options.highLat) { // high latency
+					latency = 'high';
+				}
+			} else if (options.type === 'bandwidth') {
+				bw = 102.4 / ((delta / 1000) / 2); // in kB/s
+				if (bw <= options.lowBw) { // low bandwidth
+					bw = 'low';
+				} else if (bw <= options.mediumBw) {
+					bw = 'medium';
+				} else if (bw <= options.highBw || bw > options.highBw) {
+					bw = 'high';
+				}
+			} else if (options.type === 'bandwidth latency' || options.type === 'latency bandwidth') {
+				bw = 102.4 / ((delta / 1000) / 2); // in kB/s
+				if (bw <= options.lowBw) { // low bandwidth
+					bw = 'low';
+				} else if (bw <= options.mediumBw) {
+					bw = 'medium';
+				} else if (bw <= options.highBw || bw > options.highBw) {
+					bw = 'high';
+				}
+				if (delta <= options.lowLat) { // low latency
+					latency = 'low';
+				} else if (delta <= options.mediumLat) { // medium latency
+					latency = 'medium';
+				} else if (delta <= options.highLat || delta > options.highLat) { // high latency
+					latency = 'high';
+				}
 			}
 			// now load the arrOfSrc and append to target based on latency
 			arrOfSrc.forEach(function (el) {
-				if (el.latency === latency || el.latency === 'all') {
+				if (options.type === 'latency' && ( el.latency === latency || el.latency === 'all' )) {
+					var element = document.createElement(el.tag);
+					element.src = el.src;
+					if (!el.targetElement) {
+						return $(element).appendTo(options.targetElement);
+					}
+					$(element).appendTo(el.targetElement);
+				} else if (options.type === 'bandwidth' && ( el.bandwidth === bw || el.bandwidth === 'all' )) {
+					var element = document.createElement(el.tag);
+					element.src = el.src;
+					if (!el.targetElement) {
+						return $(element).appendTo(options.targetElement);
+					}
+					$(element).appendTo(el.targetElement);
+				} else if ((options.type === 'bandwidth latency' || options.type === 'latency bandwidth') && ( (el.bandwidth === bw || el.bandwidth === 'all') && (el.latency === latency || el.latency === 'all') )) {
 					var element = document.createElement(el.tag);
 					element.src = el.src;
 					if (!el.targetElement) {
